a35ed84d7c25fafd4f682386d75c29511e14aeb9
Harmi Sutariya <sutariyaharmi@gmail.com>
2024-08-09 10:43:28 +0530

    9-8-24

--- lecture1.js
+++ lecture1.js
@@ -1,13 +1,25 @@
 //  npm init / npm init -y (default)
 
 const express = require('express');
-
+const data =  require('./friend.json')
 // http.createServer();
 const server = express(); 
+// console.log(data);
 
 // CRUD -> Create(POST) , Read(GET) , Update(PUT , PATCH) , Delete(DELETE)
 
-server.get("/" , (req , res) => {
+let middleware = (req , res , next)=>{
+    if(req.query.password === ' 1234'){
+        console.log('success');
+        next(); 
+    }else{
+        return res.json({message : 'Inccorct Way!!'})
+    }
+}
+
+// server.use(middleware)
+
+server.get("/" ,middleware, (req , res) => {
     res.write('Welcome to Express Server');
     res.end();
 })
@@ -49,16 +61,18 @@ server.delete("/Admin" , (req , res) => {
     res.json({message : 'Admin Delete Method'})
 })
 
+server.get("/friend", (req,res)=>{
+    res.status(200);
+    res.json(JSON.parse(data));
+})
 
 server.listen(1234 , () =>{
     console.log('server start at http://localhost:1234');  
 });
 
 /*
- 
   git checkout -b gk-1
   git add . 
   git commit -m "your commit"
   git push -u origin gk-1
-
 */  
