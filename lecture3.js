7fa7373a029914068e935188398da726a5b78eee
Harmi Sutariya <sutariyaharmi@gmail.com>
2024-08-13 10:14:43 +0530

    13-8-24

--- lecture3.js
+++ lecture3.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const products = require('./product.json')
+const products = require('./product.json');
+const users = require('./users.json')
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -64,6 +65,70 @@ app.delete("/product/:id" , (req , res)=>{
     products    .splice(productIndex , 1);
     res.json({product , message : " product delete successfully..."});
 })
+
+// ======================================================================================
+// ======================================================================================
+
+// CRUD
+
+// Add new users - Create
+app.post("/users" , (req ,res)=>{
+    products.push(req.body);
+    res.json({users: req.body , message : 'users Added Success'})
+});
+
+// Get All product - Read
+app.get("/users" , (req , res) => {
+    res.json(users);
+})
+
+// Get Single product - Read
+
+app.get("/users/:id" , (req , res) => {
+    let id = +req.params.id;
+    let item = users.find((users) => users.id === id );
+    res.json(item);
+});
+
+ // Replace Data - Put
+
+app.put("/users/:id" , (req ,res) => {
+    let id = +req.params.id;
+    let usersIndex = users.findIndex((users) => users.id === id);
+    // console.log(usersIndex);
+    users.splice(usersIndex , 1 , {...req.body});
+    res.json({message : "users replace success..."})
+    
+})
+
+
+// Update Data - Patch
+
+app.patch("/users/:id" , (req , res) => {
+    let id = +req.params.id;
+    let usersIndex = users.findIndex((users) => users.id === id);
+    // console.log(usersIndex);
+    const user = users[usersIndex];
+    console.log(user);
+    users.splice(usersIndex , 1 , { ...req.body});
+    res.json({message: "users update successfully.."})
+    
+
+    
+})
+
+ // Delelte Data - Delete
+
+app.delete("/users/:id" , (req , res) => {
+    let id = +req.params.id;
+    let usersIndex = users.findIndex((users) => users.id === id);
+    const user = users[usersIndex];
+    users.splice(usersIndex , 1);
+    res.json({user , message:"users delete success"});
+})
+
+
+
 
 app.listen(2711 , () => {
     console.log("server start");
